f097e7f9d9e3b555d606f05f6935f3d6f653fcc8
Kashav Madan <kshvmdn@gmail.com>
2016-01-13 15:57:47 -0500

    Fixed formatting, added Errors for promise reject()

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const open = require('open');
 const chalk = require('chalk');
 const ncp = require('copy-paste');
 
-const subreddits = ['meme', 'fffffffuuuuuuuuuuuu'];
+var subreddits = ['meme', 'memes', 'fffffffuuuuuuuuuuuu'];
 var base = 'http://reddit.com';
 
 var rndval = function(arr) { return arr[Math.floor(Math.random() * arr.length)]; }
@@ -15,7 +15,8 @@ var rndval = function(arr) { return arr[Math.floor(Math.random() * arr.length)];
 var getJSON = function(subreddit) {
 	return new Promise(function(resolve, reject) {
 		request(base + '/r/' + subreddit + '.json', function (error, response, body) {
-			if (error || response.statusCode != 200) reject();
+			if (error || response.statusCode != 200) 
+				reject('JSON request failed');
 			resolve(JSON.parse(body));
 		});
 	});
@@ -24,24 +25,26 @@ var getJSON = function(subreddit) {
 var getPost = function(json) {
 	return new Promise(function(resolve, reject) {
 		let posts = json.data.children
-		
+
 		var post = rndval(posts).data
 		// verify that post isn't text post
 		while (post.distinguished != null) {
 			post = rndval(posts).data
 		}
 
-		if (post.url == undefined || post.url == '' || post.url == null) reject();
+		if (post.url == undefined || post.url == '' || post.url == null) 
+			reject(new Error('No img URL in post'));
 		resolve(post);
 	});
 }
 
-var sr = rndval(subreddits);
+let sr = rndval(subreddits);
 
-getJSON(sr).then(function(response) {
-	return getPost(response)
+getJSON(sr).then(function(response) { 
+	return getPost(response) 
 }).then(function(post){
-	console.log( chalk.white('Opening post from ' + chalk.green('/r/' + sr) + '...' ) );
+	console.log( chalk.white('Loading post from ' + chalk.green('/r/' + sr) + '...' ) );
+	// open img url, copy thread permalink to clipboard
 	open(post.url); ncp.copy(base + post.permalink);
 	process.exit(0);
 }).catch(function(err) {
