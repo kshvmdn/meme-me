4277ec05bcefacb1e72308ddf09e68d16171c3a9
Kashav Madan <kshvmdn@gmail.com>
2016-06-13 10:21:28 -0400

    Refactor

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
 #! /usr/bin/env node
 "use strict";
 
-const Promise = require('bluebird'); 
+const Promise = require('bluebird');
 const request = require('request');
-const open = require('open'); 
+const open = require('open');
 const chalk = require('chalk');
 const ncp = require('copy-paste');
 
-var subreddits = ['meme', 'memes', 'fffffffuuuuuuuuuuuu'];
-var base = 'http://reddit.com';
+const subreddits = ['meme', 'memes', 'fffffffuuuuuuuuuuuu'];
+const base = 'http://reddit.com';
 
-var rndval = function(arr) { return arr[Math.floor(Math.random() * arr.length)]; }
+const rndVal = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-var getJSON = function(subreddit) {
-	return new Promise(function(resolve, reject) {
-		request(base + '/r/' + subreddit + '.json', function (error, response, body) {
-			if (error || response.statusCode != 200) 
-				reject('JSON request failed');
-			resolve(JSON.parse(body));
-		});
-	});
+const getJSON = (subreddit) => {
+  return new Promise((resolve, reject) => {
+    let url = `${base}/r/${subreddit}.json?limit=100`;
+    request({
+      url: url,
+      json: true
+    }, (error, response, body) => {
+      if (error || response.statusCode != 200) {
+        reject(new Error('Couldn\'t retrieve data.'));
+      }
+      resolve(body);
+    });
+  });
 }
 
-var getPost = function(json) {
-	return new Promise(function(resolve, reject) {
-		let posts = json.data.children
+const getPost = (json) => {
+  return new Promise((resolve, reject) => {
+    let posts = json.data.children;
 
-		var post = rndval(posts).data
-		// verify that post is img/gif (i.e. not text post)
-		while (post.distinguished != null) {
-			post = rndval(posts).data
-		}
+    // Verify that the post isn't a self post (i.e. not a meme)
+    let post;
+    do {
+      post = rndVal(posts).data;
+    } while (post.distinguished !== null);
 
-		if (post.url == undefined || post.url == '' || post.url == null) 
-			reject(new Error('No img URL in post'));
-		resolve(post);
-	});
+    if (post.url === undefined || post.url === '') {
+      reject(new Error('Couldn\'t find img/gif URL.'));
+    }
+    resolve(post);
+  });
 }
 
-let sr = rndval(subreddits);
+let subreddit = rndVal(subreddits);
 
-getJSON(sr).then(function(response) { 
-	return getPost(response) 
-}).then(function(post){
-	console.log( chalk.white('Loading post from ' + chalk.green('/r/' + sr) + '...' ) );
-	// open img url, copy thread permalink to clipboard
-	open(post.url); ncp.copy(base + post.permalink);
-	process.exit(0);
-}).catch(function(err) {
-	console.log( chalk.white( chalk.red('Oops! ') +  chalk.bold('MemeME') + ' failed, try again!') );
-	process.exit(1);
-});
+return getJSON(subreddit)
+  .then((response) => getPost(response))
+  .then((post) => {
+    console.log(chalk.white(`Loading post from ${chalk.green('/r/' + subreddit)}...`));
+    open(post.url); // Open submission URL
+    ncp.copy(`${base}${post.permalink}`); // Copy thread permalink to clipboard
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.log(chalk.white(`${chalk.red('Oops!')} ${chalk.bold('MemeME')} failed, try again!`));
+    process.exit(1);
+  });
